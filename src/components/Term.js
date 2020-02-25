1d5a5078d71bd5c8b79f165ae23de834c0c25214
Andrew Davisson <akdavisson4@gmail.com>
2020-02-24 22:03:15 -0700

    Added terminal and updated appearance

---
+++ src/components/Term.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Terminal from 'terminal-in-react';
+
+const Term = () => {
+  return (
+    <div id="terminal">
+      <Terminal
+        color='red'
+        backgroundColor='lightblue'
+        barColor='black'
+        allowTabs={false}
+        hideTopBar={true}
+        style={{fontweight: "bold", fontSize: "1em"}}
+      />
+    </div>
+  );
+}
+ 
+export default Term;
---
+++ src/containers/Game.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Term from '../components/Term';
+
+const Game = () => {
+  return (
+    <div id="game">
+      <h1>Text Game</h1>
+      <Term />
+    </div>
+  );
+}
+ 
+export default Game;
