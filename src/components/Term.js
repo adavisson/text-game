c05b6db2f6ffbe03fc3d016330710196b38eeb98
Andrew Davisson <akdavisson4@gmail.com>
2020-02-25 09:17:09 -0700

    trying different terminal emulator

--- src/components/Term.js
+++ src/components/Term.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import Terminal from 'terminal-in-react';
+import ReactTerminal from 'react-terminal-component';
 
 const Term = (props) => {
   return (
     <div id="terminal">
-      <Terminal
-        color='red'
-        backgroundColor='black'
-        allowTabs={false}
-        hideTopBar={true}
-        promptSymbol={`\u2620`}
-        watchConsoleLogging={true}
-        style={{fontFamily: 'Krona One', fontweight: "bold", fontSize: "1em"}}
-        commands={{
-          'start': props.startGame
-        }}
-        descriptions={{
-          'start': "Start the game."
-        }}
-      />
+      <ReactTerminal />
     </div>
   );
 }
