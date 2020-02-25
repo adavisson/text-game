7127a675c73613c1c9930403ed1b7f0033cbfd23
Andrew Davisson <akdavisson4@gmail.com>
2020-02-24 22:47:29 -0700

    trying to readline from terminal

--- src/components/Term.js
+++ src/components/Term.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import Terminal from 'terminal-in-react';
 
-const Term = () => {
+const Term = (props) => {
   return (
     <div id="terminal">
       <Terminal
         color='red'
-        backgroundColor='lightblue'
-        barColor='black'
+        backgroundColor='black'
         allowTabs={false}
         hideTopBar={true}
+        promptSymbol={`\u2620`}
+        watchConsoleLogging={true}
         style={{fontFamily: 'Krona One', fontweight: "bold", fontSize: "1em"}}
+        commands={{
+          'start': props.startGame
+        }}
+        descriptions={{
+          'start': "Start the game."
+        }}
       />
     </div>
   );
--- src/containers/Game.js
+++ src/containers/Game.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Term from '../components/Term';
+const readline = require('readline');
 
 const Game = () => {
+
+  const startGame = () => {
+    console.log("Enter Name: ");
+    let name = readline();
+    console.log(`Hello ${name}`);
+  }
+
   return (
     <div id="game">
       <h1>Text Game</h1>
-      <Term />
+      <Term startGame={startGame} />
     </div>
   );
 }
